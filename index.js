e4bddc67c533b9298313a1140a4e56aa00cd60e4
tanvirulislam912 <mdtanvir1109040@gmail.com>
2022-11-23 16:51:00 +0500

    first-commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
@@ -10,14 +11,45 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.sjiasjx.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjiasjx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+  try{
+      const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+      const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+      
+      app.get('/appointmentOptions', async(req,res) =>{
+        const date = req.query.date;
+        const query = {};
+        const options = await appointmentOptionCollection.find(query).toArray();
+        const bookingQuery = {appointmentDate: date}
+        const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+
+        options.forEach(option => {
+          const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+          const bookedSlots = optionBooked.map(book => book.slot)
+          console.log(date, option.name, bookedSlots);
+        })
+        res.send(options);
+      });
+      
+
+      app.post('/bookings', async(req, res) =>{
+        const booking = req.body
+        
+        const result = await bookingsCollection.insertOne(booking);
+        res.send(result);
+      })
+
+  }
+  finally{
+
+  }
+
+}
+run().catch(console.log);
 
 
 
