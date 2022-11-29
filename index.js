3171af569efe6615415c03e50be83fc5c4688ea5
tanvirulislam912 <mdtanvir1109040@gmail.com>
2022-11-29 21:04:39 +0500

    first commit

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function run() {
     const bookingsCollection = client.db("doctorsPortal").collection("bookings");
     const usersCollection = client.db("doctorsPortal").collection("users");
     const doctorsCollection = client.db("doctorsPortal").collection("doctors");
+    const paymentsCollection = client.db("doctorsPortal").collection("payments");
 
     const verifyAdmin =async (req, res, next) =>{
         const decodedEmail = req.decoded.email;
@@ -192,6 +193,23 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    app.post('/payments',async (req, res) =>{
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = {_id: ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          paid: true,
+          transactionId: payment.transactionId
+
+
+        }
+      }
+      const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+      res.send(result);
     })
 
     app.get("/jwt", async (req, res) => {
